ddf78c8d919f5f4849bb72d3c914f046fee5e282
Luca Strebel <luca.strebel@jmc-software.ch>
2021-12-04 11:03:35 +0100

    day 4

---
+++ days/4/part2.js
@@ -0,0 +1,34 @@
+const input = require('fs').readFileSync(__dirname + '\\data.in').toString();
+
+const split = input.split('\n\n');
+
+const draw = split[0].split(',').map(Number);
+
+const numbers = new Array(100).fill(0).map((e, i) => {
+    return {
+        number: i,
+        index: draw.indexOf(i)
+    }
+});
+
+const cards = split.slice(1).map(e => {
+    const entries = e.split('\n').map(e => e.split(' ').filter(e => e).map(e => numbers[e]));
+
+    const rows = entries.map(e => e.reduce((a, b) => a.index > b.index ? a : b));
+    const columns = entries[0].map((e, i) => entries.map(e => e[i])).map(e => e.reduce((a, b) => a.index > b.index ? a : b));
+
+    return {
+        entries,
+        result: [...rows, ...columns].reduce((a, b) => a.index < b.index ? a : b),
+        rows,
+        columns
+    } 
+});
+
+const loser = cards.reduce((a, b) => a.result.index > b.result.index ? a : b);
+const numbersLeftOnBoard = loser.entries
+    .reduce((a, b) => a.concat(...b), [])
+    .filter(e => e.index > loser.result.index)
+    .reduce((a, b) => a + b.number, 0)
+
+console.log(loser.result.number * numbersLeftOnBoard);
