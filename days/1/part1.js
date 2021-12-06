503fa5eb5285e241fcbfbf37937b574141c9be27
LucStr <25279790+LucStr@users.noreply.github.com>
2021-12-07 00:45:14 +0100

    Make Setup OS interchangeable

--- days/1/part1.js
+++ days/1/part1.js
@@ -1,7 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-
-const data = fs.readFileSync(path.join(__dirname, 'data.in')).toString().split('\n').map(Number);
+const data = require('fs').readFileSync(require('path').join(__dirname, 'data.in')).toString().split('\n').map(Number);
 
 const result = data.filter((v, i, a) => i > 1 && v > a[i - 1]).length
 
--- days/1/part2.js
+++ days/1/part2.js
@@ -1,7 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-
-const data = fs.readFileSync(path.join(__dirname, 'data.in')).toString().split('\n').map(Number);
+const data = require('fs').readFileSync(require('path').join(__dirname, 'data.in')).toString().split('\n').map(Number);
 
 const result = data.filter((v, i, a) => i > 3 && v > a[i - 3]).length
 
