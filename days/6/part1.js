2d0fce091af9982d6446b6034b8ddae2d5b5c3bf
Luca Strebel <luca.strebel@jmc-software.ch>
2021-12-06 09:50:26 +0100

    day 6

---
+++ days/6/part1.js
@@ -0,0 +1,18 @@
+const data = require('fs').readFileSync(__dirname + '\\data.in').toString();
+
+const fishes = data.split(',').map(Number).reduce((a, b) => {
+     a[b]++;
+
+     return a;
+}, new Array(9).fill(0));
+
+for(var i = 0; i < 80; i++){
+    const growth = fishes.shift();
+
+    fishes[6] += growth;
+    fishes[8] = growth;
+}
+
+const result = fishes.reduce((a, b) => a + b);
+
+console.log(result);
