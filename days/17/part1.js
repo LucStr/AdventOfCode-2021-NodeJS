0062a41b2b114d4fbaed832eb870995bd14aee61
LucStr <25279790+LucStr@users.noreply.github.com>
2021-12-17 12:29:21 +0100

    day 17

---
+++ days/17/part1.js
@@ -0,0 +1,43 @@
+let data = require('../../get_data')(17);
+
+const [_, x1, x2, y1, y2] = data.match(/target area: x=(\-?\d+)..(\-?\d+), y=(\-?\d+)..(\-?\d+)/).map(Number);
+
+function test(x, y){
+    if((x + 1) * x / 2 < x1){
+        return false;
+    }
+
+    let xPos = 0;
+    let yPos = 0;
+
+    while((xPos < x2 && x > 0) || yPos > y1){
+        xPos += x;
+        yPos += y;
+
+        if(xPos <= x2 && xPos >= x1 && yPos >= y1 && yPos <= y2){
+            return true;
+        }
+
+        if(x > 0){
+            x--;
+        }
+
+        y--;
+    }
+
+    return false;
+}
+
+let max = 0;
+
+for(let x = 0; x <= x2; x++){
+    for(let y = max; y <= Math.abs(y1); y++){
+        if(test(x, y)){
+            max = y;
+        }
+    }
+}
+
+const result = max * (max + 1) / 2;
+
+console.log(result);
